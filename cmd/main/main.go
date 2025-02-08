4f4b0dec72fb4585df526e768308956e682a060a
swiney <mikhailovskiid@gmail.com>
2025-02-08 16:15:31 +0300

    Redirect

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,18 +2,13 @@ package main
 
 import (
 	"example.com/m/pkg/api"
-	"github.com/joho/godotenv"
+	"example.com/m/pkg/redirect"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("файл .env не найден, используются переменные окружения")
-	}
-
 	// Проверяем тип памяти(in-memory или pgsql)
 	inMemory := os.Getenv("STORAGE_TYPE")
 	if inMemory == "in-memory" {
@@ -28,6 +23,7 @@ func main() {
 	// Добавляем маршруты для API
 	apiMux.HandleFunc("/api/addurl", api.AddUrl)
 	apiMux.HandleFunc("/api/geturl", api.GetUrl)
+	apiMux.HandleFunc("/", redirect.RedirectUrl)
 
 	if err := http.ListenAndServe(":3030", apiMux); err != nil {
 		log.Fatal("Ошибка сервера:", err)
---
+++ pkg/api/api_test.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestAddUrl(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			AddUrl(tt.args.w, tt.args.r)
+		})
+	}
+}
+
+func TestGetUrl(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			GetUrl(tt.args.w, tt.args.r)
+		})
+	}
+}
