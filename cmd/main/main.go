13c8d834b53bb331addcbbf70ca5f2f8a90141dc
swiney <mikhailovskiid@gmail.com>
2025-02-08 14:07:35 +0300

    v0.1

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"example.com/m/pkg/api"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("файл .env не найден, используются переменные окружения")
+	}
+
+	// Проверяем тип памяти(in-memory или pgsql)
+	inMemory := os.Getenv("STORAGE_TYPE")
+	if inMemory == "in-memory" {
+		api.InMemory = true
+	} else {
+		if api.Errdb != nil {
+			log.Fatal(api.Errdb)
+		}
+	}
+
+	apiMux := http.NewServeMux()
+	// Добавляем маршруты для API
+	apiMux.HandleFunc("/api/addurl", api.AddUrl)
+	apiMux.HandleFunc("/api/geturl", api.GetUrl)
+
+	if err := http.ListenAndServe(":3030", apiMux); err != nil {
+		log.Fatal("Ошибка сервера:", err)
+	}
 }
